7eb661bb845c0a17415cf1b02bb9b1b6c0815d5f
gando diallo <gandoesprit@gmail.com>
2021-02-18 16:20:05 +0100

    Solution for exercice3

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,6 +1,55 @@
 //insert your pseudocode below
 /*
-here
+presa una data qualsiassi trovare il numero del giorno dell'anno
+Abbiamo due variabili 
+la prima variabile num è il numero del mese
+la seconda variabile month è il nome del mese
+prendiamo i giorni dei mesi precedenti alla nostra variabile month 
+agiungiamo la varibile num
+facciamo il print della somma
 */
 
 //Insert your code below
+
+var month, num;
+
+
+month = 'Marzo';
+num = 4;
+
+if (month == 'Gennaio') {
+  window.alert(num + 0);
+}
+if (month == 'Febbraio') {
+  window.alert(num + 31);
+}
+if (month == 'Marzo') {
+  window.alert(num + 59);
+}
+if (month == 'Aprile') {
+  window.alert(num + 90);
+}
+if (month == 'Maggio') {
+  window.alert(num + 120);
+}
+if (month == 'Giugno') {
+  window.alert(num + 151);
+}
+if (month == 'Luglio') {
+  window.alert(num + 181);
+}
+if (month == 'Agosto') {
+  window.alert(num + 212);
+}
+if (month == 'Settembre') {
+  window.alert(num + 243);
+}
+if (month == 'Ottobre') {
+  window.alert(num + 274);
+}
+if (month == 'Novembre') {
+  window.alert(num + 304);
+}
+if (month == 'Dicembre') {
+  window.alert(num + 334);
+}
