9c1f2e90fdb40f4467bceefd3b6b56eef1d3cf98
gando diallo <gandoesprit@gmail.com>
2021-02-18 15:05:08 +0100

    Solution for exercice2

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,27 @@
-//insert your pseudocode below
+
 /*
-here
+    declare a list of numeric variable sort
+    display in ascending order
+    print the sorted list
 */
 
 //insert your code below
+
+var num;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+num = [10, 5, 6];
+window.alert(num.slice().sort(listsGetSortCompare("NUMERIC", 1)));
